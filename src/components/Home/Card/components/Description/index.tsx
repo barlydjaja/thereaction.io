dae26c13bff979aa7b5a12ec755cbaf1e66e1b15
BARLY <barlydjaja@gmail.com>
2024-10-29 13:52:01 +0700

    feat: add new blog post

--- src/components/Home/Card/components/Description/index.tsx
+++ src/components/Home/Card/components/Description/index.tsx
@@ -1,10 +1,14 @@
 interface DescriptionProps {
   text: string;
+  maxLen?: number;
 }
 
-const Description = ({text}: DescriptionProps) => {
+const Description = ({text, maxLen = 200}: DescriptionProps) => {
+  const exceedMaxLen = text.length > maxLen;
+
   return (
-    <p className="mt-1 font-medium text-sm max-h-10 sm:max-h-none overflow-hidden text-ellipsis">{text}</p>
+    <p
+      className="mt-1 font-medium text-sm max-h-10 sm:max-h-none overflow-hidden text-ellipsis">{exceedMaxLen ? `${text.slice(0, maxLen)}...` : text}</p>
   );
 };
 
